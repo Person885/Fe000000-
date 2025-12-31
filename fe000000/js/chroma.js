74f4e5ab4914c6f1dd4ff8f85bdcb0be0f496237
Person885 <keyboaqwerty@gmail.com>
2025-12-31 14:08:45 -0500

    Update chroma.js

--- fe000000/js/chroma.js
+++ fe000000/js/chroma.js
@@ -14,7 +14,7 @@ let Chroma = {
   colorEffectFormulas: [
     null,
     x => Decimal.pow(new Decimal(1).add(new Decimal(x).div(64)), 2.5),
-    x => Decimal.pow(new Decimal(1).add(new Decimal(x).div(64), 0.5),
+    x => Decimal.pow(new Decimal(1).add(new Decimal(x).div(64)), 0.5),
     x => Decimal.pow((new Decimal(x).gte(256)) ? Decimal.max(new Decimal(EternityPoints.totalEPProducedThisComplexity()).log2().div(4096), 1) : 2,
       Decimal.log2(new Decimal(x).div(2)).div(4).div(2).plus(1)),
     x => Decimal.pow(EternityGenerator(8).amount().max(1), new Decimal(2).mul(Decimal.sqrt(x))),
